c2ce96a0d5c826aa32ec38bd3abcad5e9cd69146
Yash020405 <yashaga0204@gmail.com>
2024-07-03 09:46:12 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+let courses = [
+    { id: 1, name: 'java' },
+    { id: 2, name: 'javascript' },
+    { id: 3, name: 'python' }
+]
+
+app.get('/courses', (req, res) => {
+    res.json(courses);
+});
+
+app.listen(3000,() => console.log('Listening on port 3000...'));
